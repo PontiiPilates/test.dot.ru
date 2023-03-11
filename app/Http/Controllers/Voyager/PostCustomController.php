bf5424e9e24ca0d32c16b1e34a0379298a2170cc
PontiiPilates <s.m.leshukov@gmail.com>
2023-03-11 19:23:00 +0700

    Adding custom controller and custom view

---
+++ app/Http/Controllers/Voyager/PostCustomController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Voyager;
+
+use Exception;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use TCG\Voyager\Database\Schema\SchemaManager;
+use TCG\Voyager\Events\BreadDataAdded;
+use TCG\Voyager\Events\BreadDataDeleted;
+use TCG\Voyager\Events\BreadDataRestored;
+use TCG\Voyager\Events\BreadDataUpdated;
+use TCG\Voyager\Events\BreadImagesDeleted;
+use TCG\Voyager\Facades\Voyager;
+use TCG\Voyager\Http\Controllers\Traits\BreadRelationshipParser;
+
+class PostCustomController extends \TCG\Voyager\Http\Controllers\VoyagerBaseController
+{
+    //
+}
