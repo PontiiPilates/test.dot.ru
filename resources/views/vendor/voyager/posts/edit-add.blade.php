a87bf108ba5bc70a811a0f47d46c738b21df0aac
PontiiPilates <s.m.leshukov@gmail.com>
2023-03-11 20:06:34 +0700

    Adding some customizations

--- resources/views/vendor/voyager/posts/edit-add.blade.php
+++ resources/views/vendor/voyager/posts/edit-add.blade.php
@@ -57,9 +57,10 @@
 @section('page_title', __('voyager::generic.'.($edit ? 'edit' : 'add')).' '.$dataType->getTranslatedAttribute('display_name_singular'))
 
 @section('page_header')
-    <h1 class="page-title">
+    <h1 class="page-title" style="color: blue">
         <i class="{{ $dataType->icon }}"></i>
-        {{ __('voyager::generic.'.($edit ? 'edit' : 'add')).' '.$dataType->getTranslatedAttribute('display_name_singular') }}
+        {{-- {{ __('voyager::generic.'.($edit ? 'edit' : 'add')).' '.$dataType->getTranslatedAttribute('display_name_singular') }} --}}
+        This is custom view!
     </h1>
     @include('voyager::multilingual.language-selector')
 @stop
